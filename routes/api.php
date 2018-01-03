34f1df226127f02522bd631f5feacfb1c9b2b6d2
tamnx <nguyen.xuan.tam@framgia.com>
2018-01-03 09:56:31 +0700

    Change jwt

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 
-Route::group(['middleware' => ['jwt-auth']], function () {
+Route::group(['middleware' => ['jwt.auth']], function () {
     Route::get('/', 'AuthController@index');
     Route::resource('items', ItemController::class);
 });
